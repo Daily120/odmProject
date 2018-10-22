658f4f24c6ebd2a74bcb3a0d730e70ee84ab5d07
Yerassyl <erasyl.boy@gmail.com>
2018-10-22 21:35:15 +0600

    changes to app classes have been made

--- origin/odm-project-frontend/src/App.js
+++ origin/odm-project-frontend/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import SideBar from './components/SideBar/SideBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App App-header">
          <div id="welcomeMessage">Hello</div>
          <img src="https://media.giphy.com/media/jWexOOlYe241y/giphy.gif" alt="sssss"/>
-        </header>
       </div>
     );
   }
