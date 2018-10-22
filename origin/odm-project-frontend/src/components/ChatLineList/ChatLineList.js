699cec5126cbd2cac2c73c28d570a235d101101c
Daily120 <forzhacker@mail.ru>
2018-10-22 22:19:09 +0200

    Created component - list of dialogs

---
+++ origin/odm-project-frontend/src/components/ChatLineList/ChatLineList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ChatLineList.css';
+import ChatLine from '../ChatLine/ChatLine';
+
+const ChatLineList = ({ chatList }) => {
+    const chatLineArray = chatList.map((chat, index) =>{
+        return <ChatLine key={index} chatName={chat.chatName} chatImage={chat.chatImage} lastMessage={chat.lastMessage} unreadedMessage={chat.unreadedMessage} /> //when adding elements through a loop, it needs to have a key prop in order to be recognisible by react
+    })
+
+    return (
+        <div>
+            {chatLineArray}
+        </div>
+    )
+}
+
+export default ChatLineList;
