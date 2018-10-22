325a84ca054061668f3015aacfea5d357fa6cc4a
Daily120 <37640497+Daily120@users.noreply.github.com>
2018-10-22 22:22:19 +0200

    Feature/chat line list (#9)
    * Created component - list of dialogs

--- origin/odm-project-frontend/src/App.js
+++ origin/odm-project-frontend/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import SideBar from './components/SideBar/SideBar';
-import ChatLine from './components/ChatLine/ChatLine';
 import logo from './components/ChatLine/favicon.ico';
 import face from './components/ChatLine/face.png';
 import postmal from './components/ChatLine/postmal.png';
+import ChatLineList from './components/ChatLineList/ChatLineList';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.chat = [{chatName: "Your Chat Bot", chatImage: logo, lastMessage: "Hello, how are you?", unreadedMessage: true},
+                  {chatName: "Face", chatImage: face, lastMessage: "Я выпустил новый альбом. Эш...", unreadedMessage: true},
+                  {chatName: "Post Malone", chatImage: postmal, lastMessage: "Listen to my new single!", unreadedMessage: true}]
+  }
+  
   render() {
     return (
       <div className="App App-header">
-        <ChatLine chatName="Your Chat Bot" chatImage={logo} lastMessage="Hello, how are you?" unreadedMessage={true}/>
-        <ChatLine chatName="Face" chatImage={face} lastMessage="Я выпустил новый альбом. Эш..." unreadedMessage={true}/>
-        <ChatLine chatName="Post Malone" chatImage={postmal} lastMessage="Listen to my new single!" unreadedMessage={true}/>
+        <ChatLineList chatList={this.chat} />
         <div id="welcomeMessage">Hello</div>
         <img src="https://media.giphy.com/media/jWexOOlYe241y/giphy.gif" alt="sssss"/>
       </div>
