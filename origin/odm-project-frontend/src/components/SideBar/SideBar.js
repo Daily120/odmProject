7a3cdb85e3c4857d606016109aac4e8a84e495a5
Yerassyl <erasyl.boy@gmail.com>
2018-10-23 03:52:13 +0600

    sidebar done , logo done , name done

--- origin/odm-project-frontend/src/components/SideBar/SideBar.js
+++ origin/odm-project-frontend/src/components/SideBar/SideBar.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './SideBar.css';
+import image from './img/asdf.jpg'
+import './animate.css';
+
+const bounce = () => {
+    let element = document.getElementsByClassName('mennenger')[0];
+    element.classList.toggle('animated');
+    element.classList.toggle('infinite');
+    element.classList.toggle('bounce');
+}
 
 const SideBar = () => {
     return (
         <div>
-
+            <div>
+                <a class="mennenger" onMouseEnter={bounce} onMouseLeave={bounce} href="#">Mennenger</a>
+                <ul class="navigation">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Donation</a></li>
+                    <li><a href="#">Sign In</a></li>
+                    <li><a href="#">Register</a></li>
+                </ul>
+            </div>
+            <img src={image} id="App-logo-spin" alt="logo" />
         </div>
     )
 }
