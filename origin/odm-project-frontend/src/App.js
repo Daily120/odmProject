ac65ed7c06d7e082137765ead479ef06674d72b4
Daily120 <37640497+Daily120@users.noreply.github.com>
2018-10-23 11:52:07 +0200

    Development (#11)
    * Deleted everything useless
    
    * We should start from here
    
    * changes to app classes have been made
    
    * Created ChatLine component
    
    * Feature/chat line list (#9)
    
    * Created component - list of dialogs
    
    * sidebar done , logo done , name done (#10)
    
    Merging our work to master

--- origin/odm-project-frontend/src/App.js
+++ origin/odm-project-frontend/src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './components/SideBar/SideBar';
+import logo from './components/ChatLine/favicon.ico';
+import face from './components/ChatLine/face.png';
+import postmal from './components/ChatLine/postmal.png';
+import ChatLineList from './components/ChatLineList/ChatLineList';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.chat = [{chatName: "Your Chat Bot", chatImage: logo, lastMessage: "Hello, how are you?", unreadedMessage: true},
+                  {chatName: "Face", chatImage: face, lastMessage: "Я выпустил новый альбом. Эш...", unreadedMessage: true},
+                  {chatName: "Post Malone", chatImage: postmal, lastMessage: "Listen to my new single!", unreadedMessage: true}]
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App App-header">
+        <ChatLineList chatList={this.chat} />
+        <div id="welcomeMessage">Hello</div>
+        <img src="https://media.giphy.com/media/jWexOOlYe241y/giphy.gif" alt="sssss"/>
       </div>
     );
   }
